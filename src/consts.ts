750f04579e81322b11cda0da9ff899d009c05952
Luminus <eleline.dev@gmail.com>
2023-12-19 02:51:27 +0900

    タイポ

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Luminus';
 export const SITE_AUTHOR = 'Luminus';
-export const SITE_DESCRIPTION = '趣味であるプラグラミングやゲームについての記事を書いていきます。更新は気まぐれですが、どうか許してください。';
+export const SITE_DESCRIPTION = '趣味であるプログラミングやゲームについての記事を書いていきます。更新は気まぐれですが、どうか許してください。';
 export const SITE_URL = 'https://luminus.dev';
